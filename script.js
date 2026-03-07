28977c8c307fe207b5fba4b93ab63143fdabcd3d
SRK SHADHIN <mt6w6v6x5b@privaterelay.appleid.com>
2026-03-08 03:32:39 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,62 @@
 const dateInput = document.getElementById("date-input");
 const calculateBtn = document.getElementById("calculateBtn");
+
 const years = document.getElementById("years");
 const months = document.getElementById("months");
 const days = document.getElementById("days");
 
+function calculateAge(dob){
 
-const calculateAge = (dob)=>{
+let today = new Date();
+let birth = new Date(dob);
 
-    let currentDate = new Date();
-    let userDob = new Date(dob);
+let y = today.getFullYear() - birth.getFullYear();
+let m = today.getMonth() - birth.getMonth();
+let d = today.getDate() - birth.getDate();
 
-    let year = currentDate.getFullYear() - userDob.getFullYear();
-    let month = currentDate.getMonth() - userDob.getMonth();
-    let day = currentDate.getDate() - userDob.getDate();
+if(d < 0){
+m--;
+d += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+}
 
-    if(day < 0){
-        month--;
-        day += new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
-    }
+if(m < 0){
+y--;
+m += 12;
+}
 
-    if(month < 0){
-        year--;
-        month += 12;
-    }
-
-    years.innerText = year;
-    months.innerText = month;
-    days.innerText = day;
+animateNumber(years,y);
+animateNumber(months,m);
+animateNumber(days,d);
 
 }
 
-calculateBtn.addEventListener("click", ()=>{
-    let inpDate = dateInput.value;
-    if(!inpDate){
-        alert("Please select a date first!")
-        return
-    }
+function animateNumber(element,value){
 
-    calculateAge(inpDate)
-})
+let start = 0;
+
+let interval = setInterval(()=>{
+
+element.innerText = start;
+
+if(start >= value){
+clearInterval(interval)
+}
+
+start++;
+
+},20)
+
+}
+
+calculateBtn.addEventListener("click",()=>{
+
+let dob = dateInput.value;
+
+if(!dob){
+alert("Please select a date");
+return;
+}
+
+calculateAge(dob);
+
+});
